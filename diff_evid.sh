fbb8484225b39e2b3e5686f2f7ff8f0d1361ae7f
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-07-27 22:06:02 +0800

    Delete diff_evid.sh

--- diff_evid.sh
+++
@@ -1,1 +0,0 @@
-python3 main.py --plm='bert' --type='lora' --train_file='datasets/preprocessed/SBERT/SBERT_train_th08.json' --valid_file='datasets/preprocessed/SBERT/SBERT_dev_th08.json' --test_file='datasets/preprocessed/SBERT/SBERT_test_th08.json' > results/230621/SBERT/SBERT_th08_LORA_1.log
