3fbf0a0cd9dbba0ab2eb8baefd8055edc0776700
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-08-05 12:52:14 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,19 @@
 from tqdm import tqdm
 import json
 
-dataset = json.load(open('./datasets/unpreprocess/test.json', 'r', encoding='utf-8'))
-save_file = open('datasets/preprocessed/test2.json', 'w', encoding='utf-8')
+# ------------------------init parameters----------------------------
+parser = argparse.ArgumentParser(description='Preprocess Datasets')
+parser.add_argument('--dataset', type=str, default="'./datasets/unpreprocess/test.json', help='dataset path')
+parser.add_argument('--save_file', type=str, default='datasets/preprocessed/test.json', help='save file path')
+
+args = parser.parse_args()
+dataset = json.load(open(args.dataset, 'r', encoding='utf-8'))
+save_file = open(args.save_file, 'w', encoding='utf-8')
 
 for data in tqdm(dataset, desc='Preprocess...'):
     claimId = data['claimId']
     claim = data['claim']
-    evidences = [data['gold evidence'][str(i)]['text'] for i in range(5)]
+    evidences = [data['gold evidence'][str(i)]['text'] for i in range(5)] 
     evidences = ''.join(evidences)
     label = data['label']
     data = json.dumps({'claimId': claimId, 'claim': claim, 'evidences': evidences, 'label': label}, ensure_ascii=False)
